bc6524d78ee321bae5630428c9e3435a450e1c85
ct101apcs <npctri.cs22@gmail.com>
2023-03-12 15:56:35 +0700

    Adjust to try and catch

--- Function/CreateSchoolYear/CreateSchoolYear.cpp
+++ Function/CreateSchoolYear/CreateSchoolYear.cpp
@@ -7,12 +7,17 @@ int inputSchoolStartYear() {
     bool valid;
     int startYear;
     do {
-        startYear = intInput();
-        std::cout << "Please enter the starting year (yyyy) of the school year: ";
-        valid = (startYear > 999 && startYear < 10000);
-        if (!valid) {
-            std::cout << "Invalid start year, please enter again!\n";
+        try {
+            startYear = intInput();
+            std::cout << "Please enter the starting year (yyyy) of the school year: ";
+            valid = (startYear > 999 && startYear < 10000);
+            if (!valid) {
+                std::cout << "Invalid start year, please enter again!\n";
+            }
+        } catch (std::exception &error) {
+            std::cout << error.what();
         }
+
     } while (!valid);
     return startYear;
 }
