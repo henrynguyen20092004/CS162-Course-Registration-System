eb6c1d62993398a1f0ff75e063e8c2f333b8796f
Nguyễn Vinh Hiển <71540470+henrynguyen20092004@users.noreply.github.com>
2023-03-13 11:04:08 +0700

    Delete default params in function definition of writeFile

--- Function/OpenFile/OpenFile.cpp
+++ Function/OpenFile/OpenFile.cpp
@@ -15,7 +15,7 @@ void readFile(std::ifstream &fin, const std::string &filePath) {
 }
 
 void writeFile(std::ofstream &fout, const std::string &filePath,
-               std::ios::openmode openmode = std::ios::out) {
+               std::ios::openmode openmode) {
     std::string fileName = getFileName(filePath);
 
     fout.open(filePath, openmode | std::ios::out);
