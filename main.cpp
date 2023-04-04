1f9b0ec5540988579797a65299418b20ba6c7a5c
Nguyễn Vinh Hiển <71540470+henrynguyen20092004@users.noreply.github.com>
2023-04-04 16:38:45 +0700

    Init raylib

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "Function/Function.h"
+#include "raylib.h"
 
 int main() {
-    menu();
+    InitWindow(800, 450, "test");
+    CloseWindow();
+    // menu();
     system("pause");
     return 0;
 }
