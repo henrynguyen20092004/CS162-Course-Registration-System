703bf9e24f36bad51f8047f9a8e60e89cdd2dd26
hdkhoaapcs22 <122841890+hdkhoaapcs22@users.noreply.github.com>
2023-04-27 13:53:15 +0700

    Fix convert score to string function

--- Function/ConvertScoreToString/ConvertScoreToString.cpp
+++ Function/ConvertScoreToString/ConvertScoreToString.cpp
@@ -3,7 +3,11 @@
 std::string convertScoreToString(double score) {
     std::string result = std::to_string(score);
 
-    while (result.back() == '0' || result.back() == '.') {
+    while (result.back() == '0') {
+        result.pop_back();
+    }
+
+    if (result.back() == '.') {
         result.pop_back();
     }
 
